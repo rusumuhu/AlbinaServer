8fd24e63f8851f5216391dae20f5287494578671
rusumuhu <nicitagerts@yandex.ru>
2021-10-28 19:23:45 +0300

    Что-то

---
+++ Albina/Albina.DataAccess.Cor/Models/UserRto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albina.DataAccess.Cor.Models
+{
+    [Table("UserRto")]
+    class UserRto
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumberPrefix { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
